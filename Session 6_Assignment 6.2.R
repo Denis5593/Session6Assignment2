ebff192214fcdc57ff650dd85bc62c717f2a6607
Denis5593 <39754431+Denis5593@users.noreply.github.com>
2018-10-06 08:33:18 +0530

    Add files via upload

---
+++ Session 6_Assignment 6.2.R
@@ -0,0 +1,7 @@
+# 1.	Import the Titanic Dataset from the link Titanic Data Set.
+# Perform the following:
+#   a. Is there any difference in fares by different class of tickets?
+#   Note - Show a boxplot displaying the distribution of fares by class
+# b. Is there any association with Passenger class and gender?
+#   Note - Show a stacked bar chart
+
